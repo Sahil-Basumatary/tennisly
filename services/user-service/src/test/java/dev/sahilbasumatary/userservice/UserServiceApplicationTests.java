ab40d14dbbd2c4db31ff485701785f8ee3ec285d
Sahil Basumatary <Basumatarysahil03@gmail.com>
2026-02-19 12:30:34 +0000

    chore: add placeholder test and migration dir for user-service

---
+++ services/user-service/src/test/java/dev/sahilbasumatary/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sahilbasumatary.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
