91d81aeb0a6fc9adb315eb6d79954a6d735adb9c
Sahil Basumatary <Basumatarysahil03@gmail.com>
2026-02-23 17:55:13 +0000

    feat: add jpa auditing config and domain enums

---
+++ services/user-service/src/main/java/dev/sahilbasumatary/userservice/entity/MemberRole.java
@@ -0,0 +1,8 @@
+package dev.sahilbasumatary.userservice.entity;
+
+public enum MemberRole {
+    OWNER,
+    ADMIN,
+    MEMBER,
+    VIEWER
+}
---
+++ services/user-service/src/main/java/dev/sahilbasumatary/userservice/entity/PlanTier.java
@@ -0,0 +1,8 @@
+package dev.sahilbasumatary.userservice.entity;
+
+public enum PlanTier {
+    FREE,
+    BASIC,
+    PRO,
+    ENTERPRISE
+}
