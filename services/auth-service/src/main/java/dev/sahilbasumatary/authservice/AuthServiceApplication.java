304c112b5ee36ebb7bc45bf4d352aca40d7cd4a3
Sahil Basumatary <Basumatarysahil03@gmail.com>
2026-03-14 14:02:12 +0000

    feat: wire Kafka producer into auth-service

--- services/auth-service/src/main/java/dev/sahilbasumatary/authservice/AuthServiceApplication.java
+++ services/auth-service/src/main/java/dev/sahilbasumatary/authservice/AuthServiceApplication.java
@@ -3,9 +3,14 @@ package dev.sahilbasumatary.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {
+    "dev.sahilbasumatary.authservice",
+    "dev.sahilbasumatary.common.kafka"
+})
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
